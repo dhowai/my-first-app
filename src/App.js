0c0edc0e6d6fd1d7615c3d90920f9cd8dbabdb09
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2022-06-22 15:47:02 +0100

    Introduction to state and function vs class based

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
+// function based
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="app">
-      <h1>Hello World</h1>
+    <div>
+      <h1>Welcome to my counter app</h1>
+      <p>The count is: {count}</p>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
