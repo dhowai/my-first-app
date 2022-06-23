4cc45ebfe4673f3fec24a4f645bed3fd0c55572a
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2022-06-23 15:15:30 +0100

    Create joke generator with custom hook

--- src/App.js
+++ src/App.js
@@ -1,37 +1,74 @@
-// import stlyes from "./App.module.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import "./App.module.css";
+import useRandomJoke from "./useRandomJoke";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState("");
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
 
-  const addTodo = (e) => {
+  const [firstName, setFirstName] = useState("Daryl");
+  const [lastName, setLastName] = useState("Howai");
+
+  const joke = useRandomJoke(firstName, lastName);
+
+  const generateJoke = (e) => {
     e.preventDefault();
-    setTodos([...todos, input]);
-    setInput("");
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
   };
 
   return (
     <div className="app">
-      <h1>Welcome to my todo list</h1>
-      {/* <h2 className={stlyes.error}>An error occured</h2> */}
-      <form>
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          type="text"
-        />
-        <button type="submit" onClick={addTodo}>
-          Add todo
-        </button>
-      </form>
+      <center>
+        <h1>The Joke Generator</h1>
+        <h2>{joke}</h2>
 
-      <h2>List of Todos</h2>
-      {todos.map((todo) => (
-        <p>{todo}</p>
-      ))}
+        <form>
+          <input placeholder="first name" ref={firstNameRef} />
+          <input placeholder="last name" ref={lastNameRef} />
+          <button onClick={generateJoke}>Generate Joke</button>
+        </form>
+      </center>
     </div>
   );
 }
 
 export default App;
+
+// // import stlyes from "./App.module.css";
+// import { useState } from "react";
+
+// function App() {
+//   const [todos, setTodos] = useState([]);
+//   const [input, setInput] = useState("");
+
+//   const addTodo = (e) => {
+//     e.preventDefault();
+//     setTodos([...todos, input]);
+//     setInput("");
+//   };
+
+//   return (
+//     <div className="app">
+//       <h1>Welcome to my todo list</h1>
+//       {/* <h2 className={stlyes.error}>An error occured</h2> */}
+//       <form>
+//         <input
+//           value={input}
+//           onChange={(e) => setInput(e.target.value)}
+//           type="text"
+//         />
+//         <button type="submit" onClick={addTodo}>
+//           Add todo
+//         </button>
+//       </form>
+
+//       <h2>List of Todos</h2>
+//       {todos.map((todo) => (
+//         <p>{todo}</p>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
